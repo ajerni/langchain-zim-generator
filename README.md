08e3cd6ec4824f1a54d58513ebe663ac6dd1b887
„Andi <ajerni@gmail.com>
2023-08-23 10:59:13 +0200

    added streamlit frontend

--- README.md
+++ README.md
@@ -3,18 +3,21 @@
 This python script uses langchain and OpenAI to create an index.html file containing ZIM code.
 It takes the whole zimdocs.txt file (thanks to Dr. Abstract - https://zimjs.com/docs.html), splits it into smaller pieces that are embedded in a vectorsore. The query is placed in the same vectorstore and returns similar chunks from the documentation, from which a ZIM code is generated using some very clever 😉 prompt engineering.
 
+## Demo
+
+todo: link to streamlit app
+
 ## How to Use
 
-1. Git Clone: `git clone https://github.com/ajerni/langchain-zim-generator.git`
-2. Move to the new directory: `cd langchain-zim-generator`
-3. Create Virtual Environment: `python -m venv env` or `python3 -m venv env` (on Windows: `python -m venv c:\path\to\env`)
-4. Activate Virtual Environment: `source env/bin/activate` on Mac or `env\Scripts\activate` on Windows
-5. Install Dependencies: `pip install -r requirements.txt`
-6. Create a .env file containing your OpenAI Key: `echo "OPENAI_API_KEY=xyz" > .env` (replace xyz with your own OpenAI Key)
-7. In main.py line 19 change the query string to whatever you want (use Zim terms circle, label, etc.) like: `3 circles within eachother. Biggest red, middle green, smallest black`
-8. Run it: `python main.py` or `python3 main.py`
-9. Copy and paste the output into your own `index.html` --> open it as your usual ZIM file!
-10. Fork my repo and start developing until we have the ultimate ZIM-Auto-Pilot 🚀
+1. Install Python 3.10 on your machine and use a bash terminal (maybe within Visual Studio Code)
+2. Git Clone: `git clone https://github.com/ajerni/langchain-zim-generator.git`
+3. Move to the new directory: `cd langchain-zim-generator`
+4. Create Virtual Environment: `python -m venv env` or `python3 -m venv env` (on Windows: `python -m venv c:\path\to\env`)
+5. Activate Virtual Environment: `source env/bin/activate` on Mac or `env\Scripts\activate` on Windows
+6. Install Dependencies: `pip install -r requirements.txt`
+7. Create a .env file containing your OpenAI Key: `echo "OPENAI_API_KEY=xyz" > .env` (replace xyz with your own OpenAI Key) - or just enter your API Key when running the app (.env is only used for local development)
+8. Run the app: `streamlit run main.py`
+9. Fork my repo and start developing until we have the ultimate ZIM-Auto-Pilot 🚀
 
 ## Credits
 
@@ -22,6 +25,5 @@ It takes the whole zimdocs.txt file (thanks to Dr. Abstract - https://zimjs.com/
 
 ## To-Dos:
 
-- create a FastAPI endpoint to run it on Vercel, Google Cloud or so...
 - Prompts need further finetuning (i.e. make positioning work as a next step etc.)
 - later on I can think of using langchain agents to include google search for pictures which can be added to the stage etc.
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import streamlit as st
+import streamlit.components.v1 as components
 import os
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -16,63 +18,97 @@ load_dotenv()
 
 os.environ['OPENAI_API_KEY'] = os.environ.get('OPENAI_API_KEY')
 
-query = "a blue circle and above it a label with the text 'it works'"
-#query = "3 circles within eachother. Biggest red, middle green, smallest black"
-#query = "an input text field with a label 'enter your name' beside each other"
+ss = st.session_state
 
-loader = TextLoader("zimdocs.txt")
-documents = loader.load()
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
+def on_api_key_change():
+    api_key = ss.get('api_key') or os.getenv('OPENAI_API_KEY')
+    os.environ['OPENAI_API_KEY'] = api_key
 
-embeddings = OpenAIEmbeddings()
+with st.sidebar:
+    st.title('langchain-zim-generator')
+    st.markdown('''
+                ## About
+    This LLM-powered app creates html code based on the ZIM template from https://zimjs.com/code.html
+    - Source code: [github.com/ajerni/langchain-zim-generator](https://github.com/ajerni/langchain-zim-generator)
+    
+    ### The app needs a valid OpenAI Key to work. Get your own on [OpenAI](https://platform.openai.com/account/api-keys)
+    ''')
+    
+    # st.text_input('OpenAI API key', type='password', key='api_key', on_change=on_api_key_change, label_visibility="collapsed")
+    st.text_input('OpenAI API key', type='password', key='api_key', on_change=on_api_key_change)
 
-vectorstore = FAISS.from_documents(docs, embeddings)
+def main():
+    #query = "a blue circle and above it a label with the text 'it works'"
+    #query = "3 circles within eachother. Biggest red, middle green, smallest black"
+    #query = "an input text field with a label 'enter your name' beside each other"
+    st.header("ZIM code generator")
+    st.write("Enter what you want the AI to create - use ZIM terms like circle, rectangle, ... [ZIM Docs](https://zimjs.com/docs.html)")
+    st.write("If for example 'a red circle with 50px radius' gives not the desired result, be more precise like 'a red circle with 50px radius. Center it on the stage.' Play with your prompts!")
+    query = st.text_input('Examples: A blue circle within a white circle / a label with the text "Enter your name:" followed by a textinput field - etc.')
 
-llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0)
+    if query:
+        results = cenerateZIMcode(query)
+        st.code(results, language='html')
+        components.html(results, width=512, height=384)
 
-similars = vectorstore.similarity_search(query=query, k=3)
-qa_chain = load_qa_chain(llm=llm, chain_type="stuff")
-response = qa_chain.run(input_documents=similars, question=query)
+def cenerateZIMcode(query):
+    loader = TextLoader("zimdocs.txt")
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter.split_documents(documents)
 
-ZIM_TEMPLATE = """
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<title>ZIM - Code Creativity</title>
+    embeddings = OpenAIEmbeddings()
 
-<!-- zimjs.com - JavaScript Canvas Framework -->
-<script type=module>
+    vectorstore = FAISS.from_documents(docs, embeddings)
 
-import zim from "https://zimjs.org/cdn/015/zim";
+    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0)
 
-// See Docs under Frame for FIT, FILL, FULL, and TAG
-new Frame(FIT, 1024, 768, light, dark, ready);
-function ready() {
-    // given F (Frame), S (Stage), W (width), H (height)
-    // put code here
-           
-} // end ready
+    similars = vectorstore.similarity_search(query=query, k=3)
+    qa_chain = load_qa_chain(llm=llm, chain_type="stuff")
+    response = qa_chain.run(input_documents=similars, question=query)
 
-</script>
-<meta name="viewport" content="width=device-width, user-scalable=no" />
-</head>
-<body></body>
-</html>
-"""
+    ZIM_TEMPLATE = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="utf-8" />
+    <title>ZIM - Code Creativity</title>
 
-system_template="""You are a javascript expert using the zimjs framework.
-you always use this template to embed your reply: {zim_template}. You put your reply into the template after '// put code here'.
-Your have the following variables given: F (Frame), S (Stage), W (width), H (height). So instead of 'stage.' use 'S.' when you code the zim content.
-"""
-system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+    <!-- zimjs.com - JavaScript Canvas Framework -->
+    <script type=module>
 
-human_template="Create the follwoing in zim: {qa_result} and place it into your template (one line after the '// put code here'). Do not change the rest of your template. Reply with the whole index.html file."
-human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    import zim from "https://zimjs.org/cdn/015/zim";
 
-chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+    // See Docs under Frame for FIT, FILL, FULL, and TAG
+    new Frame(FIT, 1024, 768, light, dark, ready);
+    function ready() {
+        // given F (Frame), S (Stage), W (width), H (height)
+        // put code here
+            
+    } // end ready
 
-response_chain = LLMChain(llm=llm, prompt=chat_prompt)
-res = response_chain.run(zim_template=ZIM_TEMPLATE, qa_result=response)
-print(res)
+    </script>
+    <meta name="viewport" content="width=device-width, user-scalable=no" />
+    </head>
+    <body></body>
+    </html>
+    """
+
+    system_template="""You are a javascript expert using the zimjs framework.
+    you always use this template to embed your reply: {zim_template}. You put your reply into the template after '// put code here'. You never need to create a Stage.
+    You already have the following variables given: F (Frame), S (Stage), W (width), H (height). So instead of 'stage.' use 'S.' when you code the zim content.
+    """
+    system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+
+    human_template="Create the follwoing in zim: {qa_result} and place it into your template (one line after the '// put code here'). Do not change the rest of your template. Reply with the whole index.html file."
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+    response_chain = LLMChain(llm=llm, prompt=chat_prompt)
+    res = response_chain.run(zim_template=ZIM_TEMPLATE, qa_result=response)
+    print(res)
+    return res
+
+if __name__ == '__main__':
+    main()
